6ea9a3b3b1fd5debc90f59dbc6a9c0173cf566ae
XuelanNM <dinastyfloyd@gmail.com>
2016-09-09 17:32:41 +0200

    Update main.c
    Finalement putain...

--- main.c
+++ main.c
@@ -2,17 +2,21 @@
 #include <stdlib.h>
 #include "header.h"
 
-int moyenneTableau(int tableau[], int tailleTableau)
+int main(int argc, char *argv[])
+
 {
-    int resTableau = tableau[0] + tableau[1];
-    return resTableau;
+    int tableau[6] = {1, 2, 3, 4, 5, 6};
+    sommeTableau(tableau,6);
+    return;
 }
 
-int main(int argc, char *argv[])
+int sommeTableau(int tableau[], int tailleTableau)
+
 {
-    int tableau[2];
-    tableau[0] = 1;
-    tableau[1] = 2;
-    moyenneTableau(tableau,2);
-    return ;
+    int resTableau = 0, i = 0;
+    for (i=0 ; i < tailleTableau ; i++)
+    {
+           resTableau = tableau[0] + tableau[i];
+    }
+    return resTableau;
 }
