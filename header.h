2ea66a4be04f69de065a29c9ded453ae4328ad0f
XuelanNM <dinastyfloyd@gmail.com>
2016-09-09 17:33:04 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,6 +1,6 @@
 #ifndef HEADER_H_INCLUDED
 #define HEADER_H_INCLUDED
 
-int moyenneTableau(int tableau[], int tailleTableau);
+int sommeTableau(int tableau[], int tailleTableau);
 
 #endif // HEADER_H_INCLUDED
